f80556b88b36a0c9c0307bea3cfa8ca2c3beac6e
thandoncube1 <ncubethando20@gmail.com>
2025-10-20 01:47:32 -0400

    Adding the producer from the kafka confluent module.

---
+++ producer.py
@@ -0,0 +1,33 @@
+import uuid
+import json
+from confluent_kafka import Producer
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Delivery failed: {err}")
+    else:
+        print(f"✅ Delivery succeeded {msg.value().decode("utf-8")}")
+
+
+order = {
+    "order_id": str(uuid.uuid4()),
+    "user": "william",
+    "item": "mushroom pizza",
+    "quantity": 2
+}
+
+value = json.dumps(order).encode("utf-8")
+
+producer.produce(
+    topic="orders",
+    value=value,
+    callback=delivery_report
+)
+
+producer.flush()
