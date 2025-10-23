1db743051d54437fd9857547e199264403d79834
thandoncube1 <ncubethando20@gmail.com>
2025-10-22 22:19:54 -0400

    Building a consumer  for the topic orders.

---
+++ tracker.py
@@ -0,0 +1,25 @@
+import json
+from confluent_kafka import Consumer
+
+consumer_config = {
+    "bootstrap.servers": "localhost:9092",
+    "group.id": "my-group",
+    "auto.offset.reset": "earliest",
+}
+
+consumer = Consumer(consumer_config)
+
+consumer.subscribe(["orders"])
+
+print("🟢 Consumer is running and subscribed to orders topic")
+
+while True:
+    msg = consumer.poll(1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        print(f"❌ Consumer error: {msg.error()}")
+
+    value = msg.value().decode("utf-8")
+    order = json.loads(value)
+    print(f"📦 Received order: {order['quantity']} x {order['item']} from {order['user']}")
